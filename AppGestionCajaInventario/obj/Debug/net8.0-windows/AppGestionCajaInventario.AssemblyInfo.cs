0d704a266dbc99148085ad20261facb5f0485b2d
JoseFonseca2024 <jose.fonseca641@std.uni.edu.ni>
2026-03-17 23:04:41 -0600

    Resolviendo clavo

--- AppGestionCajaInventario/obj/Debug/net8.0-windows/AppGestionCajaInventario.AssemblyInfo.cs
+++ AppGestionCajaInventario/obj/Debug/net8.0-windows/AppGestionCajaInventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppGestionCajaInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7201cd02488425e8fd11337f703e4cb85549fef9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28905122bedb7e8b423557a97ea53a557e8f2200")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppGestionCajaInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppGestionCajaInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
